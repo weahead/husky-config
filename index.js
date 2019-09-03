f9b4d2c8bf0a286bf55fb2bf2d41e40e68825874
Michael Zetterberg fd. Lopez <michael@weahead.se>
2019-09-03 02:06:47 +0200

    chore(repo): Introduces helpers.

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ module.exports = {
     'pre-commit': 'lint-staged',
     'post-merge': 'package-change-checker',
     'post-rewrite': 'package-change-checker $HUSKY_GIT_STDIN',
-    'post-checkout':
-      '(test -d "$(git rev-parse --git-path rebase-merge)" || test -d "$(git rev-parse --git-path rebase-apply)" ) || package-change-checker $HUSKY_GIT_PARAMS',
+    'post-checkout': './helpers/post-checkout.sh',
   },
 };
