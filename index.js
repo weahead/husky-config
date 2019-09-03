4a813c469c693601baf80524d106b7739a3d85e7
Michael Zetterberg fd. Lopez <michael@weahead.se>
2019-09-03 02:08:01 +0200

    chore(repo): Added commit lint and generator.

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 module.exports = {
   hooks: {
     'pre-commit': 'lint-staged',
+    'prepare-commit-msg': './helpers/prepare-commit-msg.sh',
+    'commit-msg': 'commitlint -E HUSKY_GIT_PARAMS',
     'post-merge': 'package-change-checker',
     'post-rewrite': 'package-change-checker $HUSKY_GIT_STDIN',
     'post-checkout': './helpers/post-checkout.sh',
