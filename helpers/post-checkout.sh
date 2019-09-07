d59c5b5b520d05837bd591764e7630d4457d176d
Michael Zetterberg fd. Lopez <michael@weahead.se>
2019-09-07 23:29:07 +0200

    fix(post-checkout): refactor

--- helpers/post-checkout.sh
+++ helpers/post-checkout.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
-if [ ! -d "$(git rev-parse --git-path rebase-merge)" ] && [ ! -d "$(git rev-parse --git-path rebase-apply)" ]; then
-  package-change-checker ${HUSKY_GIT_PARAMS}
+# Do not run package-change-checker for checkout during a rebase
+if [ -d "$(git rev-parse --git-path rebase-merge)" ] || [ -d "$(git rev-parse --git-path rebase-apply)" ]; then
+  exit 0
 fi
+
+package-change-checker ${HUSKY_GIT_PARAMS}
