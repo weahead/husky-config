c069150e6942ce87fb8330c3cf89b17444df3687
Michael Zetterberg fd. Lopez <michael@weahead.se>
2019-09-07 23:59:46 +0200

    feat(hooks): improve rebase support

--- helpers/post-rewrite.sh
+++ helpers/post-rewrite.sh
@@ -1,7 +1,15 @@
 #!/bin/sh
 
-# The command to run is passed from index.js
-cmd="${1}"
+rebase_dir=$(git rev-parse --git-path rebase-merge)
+end=$(cat "${rebase_dir}/end")
+current=$(cat "${rebase_dir}/msgnum")
 
-# shellcheck disable=SC2086
-package-change-checker --install-cmd="${cmd}" ${HUSKY_GIT_STDIN}
+if [ "${end}" = "${current}" ]; then
+  # Rebase complete, run package-change-checker
+
+  # The command to run is passed from index.js
+  cmd="${1}"
+
+  # shellcheck disable=SC2086
+  package-change-checker --install-cmd="${cmd}" ${HUSKY_GIT_STDIN}
+fi
