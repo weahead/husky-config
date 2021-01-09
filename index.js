2401e5352807353c98fb5ecf30ca5fc2088e92f4
Michael Zetterberg fd. Lopez <michael@weahead.se>
2021-01-09 10:13:59 +0100

    feat: add support for running tests on pre-commit

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
 const fs = require('fs');
+const path = require('path');
+
 const basePath = __dirname;
 const isYarn = fs.existsSync('./yarn.lock');
 const install = `${isYarn ? 'yarn' : 'npm'} install`;
 
+const pkg = require(path.resolve('./package.json'));
+let preCommit = 'lint-staged';
+if (pkg.scripts.test) {
+  preCommit += ` && ${isYarn ? 'yarn' : 'npm'} run test`;
+}
+
 module.exports = {
   hooks: {
-    'pre-commit': 'lint-staged',
+    'pre-commit': preCommit,
     'prepare-commit-msg': `${basePath}/helpers/prepare-commit-msg.sh`,
     'commit-msg': 'commitlint -E HUSKY_GIT_PARAMS',
     'post-merge': `package-change-checker --install-cmd="${install}"`,
